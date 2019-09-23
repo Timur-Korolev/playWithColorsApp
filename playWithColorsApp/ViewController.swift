776e9482b4084accb2481858528dbb608ae416fb
Timur Korolev <timurkorolev89@gmail.com>
2019-09-23 20:14:18 +0300

    Done

--- playWithColorsApp/ViewController.swift
+++ playWithColorsApp/ViewController.swift
@@ -27,17 +27,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        configureTextFields()
+        self.hideKeyboard()
     }
 
     @IBAction func SliderAction() {
-        redLabel.text = String(roundf(redSlider.value * 100) / 100)
-        greenLabel.text = String(roundf(greenSlider.value * 100) / 100)
-        blueLabel.text = String(roundf(blueSlider.value * 100) / 100)
-        mainView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        setMainViewColor()
+        getValueFromSliders()
     }
-}
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        getValueFromTextFelds()
+        setMainViewColor()
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    }
 
-extension ViewController {
+extension ViewController: UITextFieldDelegate {
     private func setupView() {
         mainView.layer.cornerRadius = 15
         
@@ -45,8 +53,63 @@ extension ViewController {
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
         
-        redLabel.text = String(redSlider.value)
-        greenLabel.text = String(greenSlider.value)
-        blueLabel.text = String(blueSlider.value)
+        getValueFromSliders()
+        setMainViewColor()
+    }
+    
+    private func configureTextFields() {
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
+    }
+    
+    private func setMainViewColor() {
+        mainView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    private func getValueFromSliders() {
+        redLabel.text = String(roundf(redSlider.value * 100) / 100)
+        greenLabel.text = String(roundf(greenSlider.value * 100) / 100)
+        blueLabel.text = String(roundf(blueSlider.value * 100) / 100)
+        
+        redTextField.text = String(roundf(redSlider.value * 100) / 100)
+        greenTextField.text = String(roundf(greenSlider.value * 100) / 100)
+        blueTextField.text = String(roundf(blueSlider.value * 100) / 100)
+    }
+    
+    private func getValueFromTextFelds() {
+        guard let redText = redTextField.text, !redText.isEmpty  else { return }
+        if let number = Float(redText) {
+            redSlider.value = number
+            redLabel.text = String(number)
+        }
+        guard let greenText = greenTextField.text, !greenText.isEmpty  else { return }
+        if let number = Float(greenText) {
+            greenSlider.value = number
+            greenLabel.text = String(number)
+        }
+        guard let blueText = blueTextField.text, !blueText.isEmpty  else { return }
+        if let number = Float(blueText) {
+            blueSlider.value = number
+            blueLabel.text = String(number)
+        }
+    }
+}
+
+extension UIViewController
+{
+    func hideKeyboard()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(UIViewController.dismissKeyboard))
+        
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard()
+    {
+        view.endEditing(true)
     }
 }
